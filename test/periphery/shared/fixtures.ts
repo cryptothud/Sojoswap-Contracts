b834952a1035745a5339556ff802d50105b892d0
Richard Dwi <dwi@reimu>
2022-03-29 23:20:39 -0400

    remove provider

--- test/periphery/shared/fixtures.ts
+++ test/periphery/shared/fixtures.ts
@@ -1,12 +1,8 @@
-import { Wallet, Contract } from 'ethers'
+import { Wallet, Contract} from 'ethers'
 import { expandTo18Decimals } from './utilities'
 import {Web3Provider} from '@ethersproject/providers'
 import { deployContract, ERC20, IUniswapV2Pair, RouterEventEmitter, UniswapV2Factory, UniswapV2Router02, WETH9 } from '../../reexports'
-
-const overrides = {
-  gasLimit: 9999999
-}
-
+import {ethers} from 'hardhat'
 interface V2Fixture {
   token0: Contract
   token1: Contract
@@ -22,36 +18,36 @@ interface V2Fixture {
 
 export async function v2Fixture([wallet]: Wallet[], provider: Web3Provider): Promise<V2Fixture> {
   // deploy tokens
-  const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
-  const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
-  const WETH = await deployContract(wallet, WETH9, [], overrides)
-  const WETHPartner = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
+  const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+  const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+  const WETH = await deployContract(wallet, WETH9, [])
+  const WETHPartner = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
 
   // deploy V1 omitted
 
   // deploy V2
-  const factoryV2 = await deployContract(wallet, UniswapV2Factory, [wallet.address], overrides)
+  const factoryV2 = await deployContract(wallet, UniswapV2Factory, [wallet.address])
 
   // deploy routers
-  const router02 = await deployContract(wallet, UniswapV2Router02, [factoryV2.address, WETH.address], overrides)
+  const router02 = await deployContract(wallet, UniswapV2Router02, [factoryV2.address, WETH.address])
 
   // event emitter for testing
-  const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [], overrides)
+  const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
 
   // deploy migrator omitted
 
   // initialize V2
-  await factoryV2.createPair(tokenA.address, tokenB.address, overrides)
+  await factoryV2.createPair(tokenA.address, tokenB.address)
   const pairAddress = await factoryV2.getPair(tokenA.address, tokenB.address)
-  const pair = new Contract(pairAddress, JSON.stringify(IUniswapV2Pair.abi), provider).connect(wallet)
+  const pair = new ethers.Contract(pairAddress, JSON.stringify(IUniswapV2Pair.abi))
 
   const token0Address = await pair.token0()
   const token0 = tokenA.address === token0Address ? tokenA : tokenB
   const token1 = tokenA.address === token0Address ? tokenB : tokenA
 
-  await factoryV2.createPair(WETH.address, WETHPartner.address, overrides)
+  await factoryV2.createPair(WETH.address, WETHPartner.address)
   const WETHPairAddress = await factoryV2.getPair(WETH.address, WETHPartner.address)
-  const WETHPair = new Contract(WETHPairAddress, JSON.stringify(IUniswapV2Pair.abi), provider).connect(wallet)
+  const WETHPair = new ethers.Contract(WETHPairAddress, JSON.stringify(IUniswapV2Pair.abi))
 
   return {
     token0,
